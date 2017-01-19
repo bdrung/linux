e56bd45cfd74fe3d7c6592dc08cbedd8b9609411
charles.park <charles.park@hardkernel.com>
2017-01-19 16:12:44 +0900

    ODROID-XU4: add odroid32 spi lcd driver.(CS-GPIO Error fix)
    Change-Id: I697ab4aa709637bb2bbccab5768798db639292c9

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -785,6 +785,10 @@ static int spidev_remove(struct spi_device *spi)
 	/* make sure ops on existing fds can abort cleanly */
 	spin_lock_irq(&spidev->spi_lock);
 	spidev->spi = NULL;
+
+	if(spi->controller->cleanup)
+		spi->controller->cleanup(spi);
+
 	spin_unlock_irq(&spidev->spi_lock);
 
 	/* prevent new opens */
